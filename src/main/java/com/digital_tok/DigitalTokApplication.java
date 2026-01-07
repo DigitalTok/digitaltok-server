2a35c5fea68879e68f0b96424a708848a908dfea
LeeJeongHeon02 <aoc05230@naver.com>
2026-01-07 17:08:24 +0900

    Initial Commit

---
+++ src/main/java/com/digital_tok/DigitalTokApplication.java
@@ -0,0 +1,13 @@
+package com.digital_tok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalTokApplication.class, args);
+	}
+
+}

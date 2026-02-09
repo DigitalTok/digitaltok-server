96e420b20f5292c5a940ece8d56ab19d0de66144
이정헌 <aoc05230@naver.com>
2026-02-10 03:11:38 +0900

    Docs: TODO 추가

--- src/main/java/com/digital_tok/device/dto/DeviceRequestDTO.java
+++ src/main/java/com/digital_tok/device/dto/DeviceRequestDTO.java
@@ -6,5 +6,6 @@ import lombok.Getter;
 @Getter
 public class DeviceRequestDTO {
     @NotBlank(message = "NFC UID는 필수입니다.")
+    // TODO: 우리 기기의 UID 형식이 맞는지 Pattern 어노테이션 통해서 검증하면 좋을듯
     private String nfcUid; // NFC UID
 }

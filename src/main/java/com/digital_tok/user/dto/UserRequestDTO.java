0bfc08f5fe83c0cbee5ad7b5520d5b79cf3fbc0a
이정헌 <aoc05230@naver.com>
2026-02-10 02:08:58 +0900

    Refactor: auth, user 도메인 DTO 검증 추가

--- src/main/java/com/digital_tok/user/dto/UserRequestDTO.java
+++ src/main/java/com/digital_tok/user/dto/UserRequestDTO.java
@@ -1,6 +1,8 @@
 package com.digital_tok.user.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 public class UserRequestDTO {
@@ -14,14 +16,21 @@ public class UserRequestDTO {
     // 2. 비밀번호 변경 요청
     @Getter
     public static class ChangePasswordDto {
+        @NotBlank(message = "현재 비밀번호를 입력해주세요.")
         private String oldPassword; // 필수 (현재 비밀번호)
+
+        @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
         private String newPassword; // 필수 (새로운 비밀번호)
     }
 
     // 3. 이메일 변경 요청
     @Getter
     public static class ChangeEmailDto {
+        @NotBlank(message = "비밀번호를 입력해주세요.")
         private String password;    // 필수 (본인 확인용)
+
+        @NotBlank(message = "새로운 이메일을 입력해주세요.")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
         private String newEmail;    // 필수 (변경할 새 이메일)
     }
 
